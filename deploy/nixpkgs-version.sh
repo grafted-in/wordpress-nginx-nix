bb2e8c42771d0f4462b768970a2e5f2daa382f67
3noch <eacameron@gmail.com>
2017-01-12 21:21:35 -0500

    Upgrade Nixpkgs and use now-included Nginx modules

--- deploy/nixpkgs-version.sh
+++ deploy/nixpkgs-version.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 export nixpkgs_channel="https://nixos.org/channels/nixpkgs-unstable"
-export nixpkgs_snapshot="https://d3g5gsiof5omrk.cloudfront.net/nixpkgs/nixpkgs-17.03pre97534.5ed1aee/nixexprs.tar.xz"
+export nixpkgs_snapshot="https://d3g5gsiof5omrk.cloudfront.net/nixpkgs/nixpkgs-17.03pre98765.6043569/nixexprs.tar.xz"
 
 if [ "$(uname)" == "Darwin" ]; then
-  export nixops_version="/nix/store/8kgw7k7z8rs2fp12253zr7f5sb4v311g-nixops-1.5pre2118_2d8b282"
+  export nixops_version="/nix/store/1gy62jcxjc09n9gk0ns4qk3d9b9kcda7-nixops-1.5pre2121_fc43d9c"
 else
-  export nixops_version="/nix/store/h2dbmlnpnh7c7k0izmglxy270gs9nmwn-nixops-1.5pre2118_2d8b282"
+  export nixops_version="/nix/store/d553achr2pvh6p8838a4shbhjpp5d6s6-nixops-1.5pre2121_fc43d9c"
 fi
 
 if [ ! -d "$nixops_version" ]; then
--- server/nginx.nix
+++ server/nginx.nix
@@ -1,57 +1,10 @@
-{ callPackage, fetchFromGitHub, fetchzip, lib, nginxModules, runCommand, stdenv
-, enablePageSpeed
-, ...
-}:
-let
-  fastcgi-cache-purge = {
-    src = fetchFromGitHub {
-      owner  = "FRiCKLE";
-      repo   = "ngx_cache_purge";
-      rev    = "2.3";
-      sha256 = "0ib2jrbjwrhvmihhnzkp4w87fxssbbmmmj6lfdwpm6ni8p9g60dw";
-    };
-  };
-
-  pagespeed = let
-    # Build instructions:
-    # https://developers.google.com/speed/pagespeed/module/build_ngx_pagespeed_from_source
-    # WARNING: This only works with Linux because the pre-built PSOL binary is only supplied for
-    # Linux.
-    # TODO: Build PSOL from source to support more platforms.
-
-    version = "1.11.33.4";
-
-    moduleSrc = fetchFromGitHub {
-      owner  = "pagespeed";
-      repo   = "ngx_pagespeed";
-      rev    = "v${version}-beta";
-      sha256 = "03dvzf1lgsjxcs1jjxq95n2rhgq0wy0f9ahvgascy0fak7qx4xj9";
-    };
-
-    psol = fetchzip {
-      url    = "https://dl.google.com/dl/page-speed/psol/${version}.tar.gz";
-      sha256 = "1jq2llp0i4666rwqnx1hs4pjlpblxivvs1jkkjzlmdbsv28jzjq8";
-    };
-
-    ngx_pagespeed = runCommand
-      "ngx_pagespeed"
-      {
-        meta = { platforms = stdenv.lib.platforms.linux; };
-      }
-      ''
-        cp -r "${moduleSrc}" "$out"
-        chmod -R +w "$out"
-        ln -s "${psol}" "$out/psol"
-      '';
-
-  in { src = ngx_pagespeed; };
-in
+{ callPackage, lib, nginxModules, enablePageSpeed, ... }:
 callPackage <nixpkgs/pkgs/servers/http/nginx/mainline.nix> {
   modules = [
       nginxModules.dav
+      nginxModules.fastcgi-cache-purge
       nginxModules.moreheaders
-      fastcgi-cache-purge
     ]
-    ++ lib.optional enablePageSpeed pagespeed
+    ++ lib.optional enablePageSpeed nginxModules.pagespeed
   ;
 }
