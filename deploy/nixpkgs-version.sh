9632b1cd446d8b870e50ef5dda85dd11ef3ca784
3noch <eacameron@gmail.com>
2017-01-26 20:09:01 -0500

    Add docs for upgrading nixpkgs and nixops

--- deploy/nixpkgs-version.sh
+++ deploy/nixpkgs-version.sh
@@ -1,7 +1,21 @@
 #!/usr/bin/env bash
+
+# Check out different Nixpkgs channels here:
+#   * http://howoldis.herokuapp.com/
+#   * https://nixos.org/channels/
+#
+# To upgrade:
+#   1. Choose a channel and click on it.
+#   2. Get the URL of the `nixexprs.tar.xz` file for the channel.
+#   3. Run `nix-preftech-zip` (you may need to install it with `nix-env -i`)
+#      on the URL to get the SHA-256 hash.
+#   4. Paste both the URL and hash below:
+
 export nixpkgs_channel="https://nixos.org/channels/nixpkgs-unstable"
 export nixpkgs_snapshot="https://d3g5gsiof5omrk.cloudfront.net/nixpkgs/nixpkgs-17.03pre98765.6043569/nixexprs.tar.xz"
 
+# See latest builds of NixOps for each platform:
+#   http://hydra.nixos.org/jobset/nixops/master#tabs-jobs
 if [ "$(uname)" == "Darwin" ]; then
   export nixops_version="/nix/store/1gy62jcxjc09n9gk0ns4qk3d9b9kcda7-nixops-1.5pre2121_fc43d9c"
 else
