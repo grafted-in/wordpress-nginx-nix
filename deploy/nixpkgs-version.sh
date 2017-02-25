9911ecbb06a3e652b6d3053e6c00391d6433a2b1
3noch <eacameron@gmail.com>
2017-02-25 12:23:33 -0500

    Upgrade nixpkgs
    Includes:
      * PHP 7.1
      * Newer Nginx
      * Newer PHP modules (like xdebug)

--- deploy/nixpkgs-version.sh
+++ deploy/nixpkgs-version.sh
@@ -7,21 +7,8 @@
 # To upgrade:
 #   1. Choose a channel and click on it.
 #   2. Get the URL of the `nixexprs.tar.xz` file for the channel.
-#   3. Run `nix-preftech-zip` (you may need to install it with `nix-env -i`)
-#      on the URL to get the SHA-256 hash.
-#   4. Paste both the URL and hash below:
+#   4. Paste the URL below for `nixpkgs_snapshot`.
 
-export nixpkgs_channel="https://nixos.org/channels/nixpkgs-unstable"
-export nixpkgs_snapshot="https://d3g5gsiof5omrk.cloudfront.net/nixpkgs/nixpkgs-17.03pre98765.6043569/nixexprs.tar.xz"
-
-# See latest builds of NixOps for each platform:
-#   http://hydra.nixos.org/jobset/nixops/master#tabs-jobs
-if [ "$(uname)" == "Darwin" ]; then
-  export nixops_version="/nix/store/1gy62jcxjc09n9gk0ns4qk3d9b9kcda7-nixops-1.5pre2121_fc43d9c"
-else
-  export nixops_version="/nix/store/d553achr2pvh6p8838a4shbhjpp5d6s6-nixops-1.5pre2121_fc43d9c"
-fi
-
-if [ ! -d "$nixops_version" ]; then
-  nix-store -r "$nixops_version"
-fi
+export nixpkgs_channel="https://nixos.org/channels/nixpkgs-unstable"  # For reference only.
+export nixpkgs_snapshot="https://d3g5gsiof5omrk.cloudfront.net/nixpkgs/nixpkgs-17.03pre101896.4a524cf/nixexprs.tar.xz"
+export nixops_version="nixops"
