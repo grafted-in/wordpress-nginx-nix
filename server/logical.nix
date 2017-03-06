b2ab708bf98c07010ab4c7668ba79ad6b44c8b81
3noch <eacameron@gmail.com>
2017-03-06 16:56:20 -0500

    Use full path to sendmail

--- server/logical.nix
+++ server/logical.nix
@@ -74,7 +74,7 @@ in {
         ; WARNING: Be sure to load opcache *before* xdebug (http://us3.php.net/manual/en/opcache.installation.php).
         zend_extension = "${pkgs.phpPackages.xdebug}/lib/php/extensions/xdebug.so"
 
-        sendmail_path = /var/setuid-wrappers/sendmail -t -i
+        sendmail_path = ${pkgs.postfix}/bin/sendmail -t -i
 
         ${import ./opcache-config.nix { enabled = appConfig.enableOpCache; }}
       '';
