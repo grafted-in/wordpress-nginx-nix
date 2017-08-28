9427efb56820f9bbf20810db514b830dd1c7c07c
3noch <eacameron@gmail.com>
2017-08-28 14:11:06 -0400

    Fix issue with too many redirects on directory pages
    c.f. https://github.com/nystudio107/nginx-craft/commit/b031eaaedec0a3415e2f3e7f1bde965207e1bc6d

--- server/nginx-config.nix
+++ server/nginx-config.nix
@@ -138,7 +138,7 @@ let
 
     # Root directory location handler
     location / {
-      try_files $uri $uri/ /index.php?$query_string;
+      try_files $uri/index.html $uri $uri/ /index.php?$query_string;
     }
 
     ${if enableFastCgiCache then fastgciCachePart.serverConfig else ""}
