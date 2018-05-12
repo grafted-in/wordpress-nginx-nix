85d690b4fdc944b1b2acc65f11848db5484c5632
Richard Marko <srk@48.io>
2018-05-12 12:53:44 +0200

    add libvirt configs

---
+++ server/logical.libvirt.nix
@@ -0,0 +1,8 @@
+import ./logical.nix (self: super: {
+  domain         = "testsite.dev";
+  host           = self.domain;
+  hostRedirects  = [];
+  enableHttps    = false;
+  enableRollback = false;
+  enableXDebug   = true;
+})
